0d2e1b1bdb46eb53a7201eb69b8a2e3a8ff6a50f
Sean Karlage <skarlage@fb.com>
2016-09-06 17:24:36 -0700

    Move bashrc to bashrc_common

--- bash/bashrc_common.sh
+++ bash/bashrc_common.sh
@@ -1,13 +1,17 @@
 # Get a nice prompt
 export PS1="[\t] \u@\h:\w\n$ "
 
-# Prevent anything from creating output that breaks utilities that use ssh as a
-# pipe
-[ -z "$PS1" ] && return
+# Personal scripts
+export PATH=$HOME/bin:$PATH
 
 # Look for man pages in /usr/local/man first
 export MANPATH=/usr/local/man:/usr/local/share/man:/usr/share/man:/usr/man
 source $HOME/.man_colors
 
-# Personal scripts
-export PATH=$HOME/bin:$PATH
+# Let's use vim for things
+export EDITOR=vim
+
+# More prompt stuff
+export CLICOLOR=1
+export GREP_OPTIONS='--color=auto'
+export LSCOLORS=GxFxCxDxBxegedabagaced
