8f3c3625a5ac801298a7ee1fcce1edddb004fb1c
Sean Karlage <skarlage@fb.com>
2016-10-11 12:50:07 -0700

    Fix linux bootstrap process

--- bootstrap_linux.sh
+++ bootstrap_linux.sh
@@ -1,2 +1,3 @@
 # Link linux bash
-bootstrap_link "bash/bash_linux.sh", "$HOME/.bashrc"
+bootstrap_link "$cur_dir/bash/bash_linux.sh" "$HOME/.bashrc"
+bootstrap_link "$cur_dir/bash/alias_linux.sh" "$HOME/.alias_linux"
